16bbfb1cc5b59b17ae2991e6c05933c5ab965af2
Jack Song <ts7fx@virginia.edu>
2017-07-27 21:04:05 -0400

    1. implemented instantSearch feature; 2. fixed bug of query changing to lower-case in list item in result panel

--- content.js
+++ content.js
@@ -27,15 +27,17 @@
     var sentence = captureGroups[i].replace(/<[^>]*>/g, ''); // substitute unwant chars with empty ones
     result[timeStamp] = sentence; // now result contains (timeStamp : sentence) key-value pairs
   } 
-  console.log(result.length);
-
   // 2. Perform keyword search
   for (time in result){
-    // do the search by linear comparison, ignore case, O(n)
-    if (result[time].toUpperCase().includes(query.toUpperCase())){
-      console.log("found keyword \""+query+"\" at timeStamp:"+time+", the corresponding sentence is: " + result[time]);
-      // inject finding
-      var li = handleResult(query, time, result[time]);
+    if (query.length == 0){
+      break;
+    }
+    var currentLine = result[time];
+    var queryLowerCase = query.toLowerCase();
+    if (currentLine.toLowerCase().indexOf(queryLowerCase) != -1){
+      var beg = currentLine.toLowerCase().indexOf(queryLowerCase);
+      var queryActualCase = currentLine.substr(beg, queryLowerCase.length);
+      var li = handleResult(queryActualCase, time, currentLine);
       document.getElementById("resultPanel").appendChild(li);
     }
   }
@@ -48,77 +50,69 @@
  * @param {String} sentence - sentence itself
  * @return {Object} listElement - ready-to-use list item
  */
- function str_pad_left(string,pad,length) {
-  // helper function to pad the string
-  return (new Array(length+1).join(pad)+string).slice(-length);
-}
 function handleResult(query, timeStamp, sentence){
-  var li = document.createElement("li");
-  li.className = "list-group-item";
-  // find query in sentence, highlight it 
-  // a temporary helper function
+  // temp helper functions
   String.prototype.replaceAll = function(strReplace, strWith) {
     // See http://stackoverflow.com/a/3561711/556609
     var esc = strReplace.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
     var reg = new RegExp(esc, 'ig');
     return this.replace(reg, strWith);
   };
+  str_pad_left = function(string,pad,length){
+    return (new Array(length+1).join(pad)+string).slice(-length);
+  };
+
+  var li = document.createElement("li");
+  li.className = "list-group-item";
+  // find query in sentence, highlight it 
   sentence = sentence.replaceAll(query,"<b style='color:#e74c3c'>"+query+"</b>"); 
   // format time
   var minutes = Math.floor(timeStamp / 60);
   var seconds = parseInt(timeStamp%60);
   var finalTime = str_pad_left(minutes,'0',2)+':'+str_pad_left(seconds,'0',2);
-  // put time + sentence together
+  // put time & sentence together
   li.innerHTML = finalTime + ' ' + sentence;
   // add event listener for click
   li.addEventListener("click", function(){
     var video = document.getElementsByTagName("video")[0];
     video.currentTime = Number(timeStamp);
   });
-  // return element
   return li;
 }
 
 /* 
- * Initiate search box
+ * Initiate search box with instant search feature
  * @return {void}
  */
  function buildSearchBox(){
-  // inject searchBox and Go button to HTML.
   var container = document.getElementById("watch-header");
   var searchBox = document.createElement('INPUT'); 
-  var button = document.createElement("BUTTON");
   var listGroup = document.createElement("UL");
   searchBox.id = "searchBox";
-  button.innerHTML = 'search!';
-  button.id = "myButton";
-  
   listGroup.id = "resultPanel";
   listGroup.className = "list-group";
-
-  button.addEventListener("click", function(){
+  
+  searchBox.addEventListener("keyup", function(){
     document.getElementById("resultPanel").innerHTML = ''; // clean the result panel for every new search
     var text = document.getElementById("searchBox").value;
     search(text);
   });
+
   container.insertBefore(listGroup, container.childNodes[0]);
-  container.insertBefore(button, container.childNodes[0]);
   container.insertBefore(searchBox, container.childNodes[0]);
 }
 
 var content = '';
-// var captureGroups = '';
 var subURL = '';
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     var crawler = new XMLHttpRequest();
     crawler.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
-        console.log("subtitle crawled for video: " + request.id);
         content = crawler.responseText;
         subURL = request.url;
-        console.log('subtitle url is: ' + subURL);
         buildSearchBox();
+        console.log('v_id: ' + request.id + ' url:' + subURL);
       }
     };
     crawler.open("GET", request.url, true);
