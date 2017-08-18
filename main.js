1542c2b0cdb1b40747601d617050bb6969d16055
Tianye Song <ts7fx@virginia.edu>
2017-08-17 21:16:04 -0700

    firing condition fix
    finally got the firing condition going well.

--- main.js
+++ main.js
@@ -1,30 +1,46 @@
+//set an interval as a pipeline for reacting to specific DOM conditions.
+
+sessionStorage['itval']=setInterval(checkList,1000);
+
+var goSignal={
+  'clickButton': false
+};
+var reactions={
+  'clickButton':clickButton
+};
+
+function checkList(){
+  //console.log('counting');
+  //list of conditions, change corresponding goSignal
+  if (document.querySelector('#my-results')==null &&
+      document.getElementsByClassName('ytp-subtitles-button')[0].clientHeight != 0 &&
+      document.getElementsByClassName('ytp-subtitles-button')[0].getAttribute('aria-pressed') == 'false'){
+    goSignal['clickButton']=true;
+  }
+  //loop through to run all reactions
+  for (key in goSignal) {
+    if(goSignal[key]){
+      reactions[key]();
+      goSignal[key]=false;
+      //console.log(key,'triggered')
+    };
+  }
+}
+function clickButton(){
+  document.getElementsByClassName('ytp-subtitles-button')[0].click();
+}
+
 /** main: whenever a new request is received, new UI & subtitle object are created */
 var myVar;
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
-    if (request.message == 'reset'){
-      myVar = setInterval(function(){
-        if (document.getElementsByClassName('ytp-subtitles-button')[0].clientHeight != 0){
-          document.getElementsByClassName('ytp-subtitles-button')[0].click();
-          console.log('button clicked once');
-          setTimeout(function(){
-            document.getElementsByClassName('ytp-subtitles-button')[0].click();
-            console.log('button clicked twice');
-          }, 500);
-        }
-        else{
-          console.log('no subtitle');
-          clearInterval(myVar);
-        }
-      }, 1000);
-    }
-    else if (request.message == 'subURL'){
-      clearInterval(myVar);
+    if (request.message == 'subURL'){
       let crawler = new XMLHttpRequest();
       crawler.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
           const c = new subtitle(crawler.responseText, request.url);
           controlPanel.addMotherBoard(c, controlPanel.addSearchBox, controlPanel.addMouseListener);
+          document.getElementsByClassName('ytp-subtitles-button')[0].click();
           //sendResponse({message: "subtitle loaded"});
           // let sender = new XMLHttpRequest();
           // sender.open("POST", "http://127.0.0.1:1234/", true);
@@ -37,6 +53,6 @@ chrome.runtime.onMessage.addListener(
       return true;
     }
     else{
-      console.log('error');
+      //console.log('error');
     }
   });
