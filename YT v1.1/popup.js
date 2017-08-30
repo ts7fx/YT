4ff3baaa1d8aa1157caed83a5b4eb4b5b7468335
Jack Song <ts7fx@virginia.edu>
2017-08-30 10:38:17 -0700

    remove redundant files

--- YT v1.1/popup.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-  document.getElementById('git').addEventListener("click", function(){ // add event listener for click
-    chrome.tabs.create({ url: 'https://github.com/ts7fx/YT' });
-  });
-});
